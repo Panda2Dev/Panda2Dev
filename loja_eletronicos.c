a0d6f69ed861aa8c729b4d00a02c8fb14505b9dd
Mateus santos <msjt098@gmail.com>
2024-09-01 09:49:36 -0300

    Criação da função de cadastro de usuarios

--- loja_eletronicos.c
+++ loja_eletronicos.c
@@ -1,34 +1,76 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main () {
-    //define tamanho das constantes
-    #define MAX_USUARIOS 10
-    #define TAMANHO_NOME 30
-    #define TAMANHO_SENHA 5
-    #define LINHAS 5
-    #define COLUNAS 3
-    #define TAMANHO_ID 7
-    #define TAMANHO_STATUS 20
-    //
+// define tamanho das constantes
+#define MAX_USUARIOS 10
+#define TAMANHO_NOME 30
+#define TAMANHO_SENHA 5
+#define LINHAS 5
+#define COLUNAS 3
+#define TAMANHO_ID 7
+#define TAMANHO_STATUS 20
+//
 
-    const char SENHA_CADASTRO = "2024";
-    //produto
-    struct Produto{
-        char id[TAMANHO_ID];
-        char nome[TAMANHO_NOME];
-        char status[TAMANHO_STATUS];
-    };
+const char SENHA_CADASTRO = "2025";
+// produto
+typedef struct
+{
+    char id[TAMANHO_ID];
+    char nome[TAMANHO_NOME];
+    char status[TAMANHO_STATUS];
+} Produto;
 
-    //usuario
-    struct Usuario {
-        char nome[TAMANHO_NOME];
-        char cargo [TAMANHO_NOME];
-        char senha [TAMANHO_SENHA];
-    };
-   
+// usuario
+typedef struct
+{
+    char nome[TAMANHO_NOME];
+    char cargo[TAMANHO_NOME];
+    char senha[TAMANHO_SENHA];
+} Usuario;
 
+// cria um array para armazena os produtos
+Produto produtos[LINHAS][COLUNAS];
+int quantidadeProdutos = 0;
+
+// cria um array para armazena os usuarios
+Usuario usuarios[MAX_USUARIOS];
+int quantidadeUsuarios = 0;
+
+// funcao para cadastar um novo usuario
+void cadastrarUsuario()
+{
+    char senha[TAMANHO_SENHA];
+
+    printf("Digite a senha de cadastro: ");
+    scanf("%s", senha);
+    getchar();
+
+    if (strcmp(senha, SENHA_CADASTRO) != 0)
+    {
+        printf("Senha incorreta! Retornando ao menu principal.\n");
+        return;
+    }
+
+    // verifica se a quantidade de usuarios excedeu o limite estabelecido.
+    if (quantidadeUsuarios >= MAX_USUARIOS)
+    {
+        printf("Limite maximo de usuarios atingido!\n");
+        return;
+    }
+
+    // recebe o nome do usuario
+    printf("Digite o nome do usuario: ");
+    scanf("%s", &usuarios[quantidadeUsuarios].nome);
+
+    printf("Digite o cargo do usuario (Admin ou Vendedor): "); // recebe o cargo do usuario
+    scanf("%s", &usuarios[quantidadeUsuarios].cargo);
+    getchar();
+
+    printf("Digite a senha (maximo 4 digitos): "); // recebe a senha do usuario
+    scanf("%4s", &usuarios[quantidadeUsuarios].senha);
+    getchar();
+
+    quantidadeUsuarios++; // incrementa a variavel em 1
+    printf("Usuario cadastrado com sucesso!\n");
 }
-
-
-
