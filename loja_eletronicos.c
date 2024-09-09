5faa0738a7b642af8698fa057edda8cf9521bcc2
DarkSocram <marcoslupus13@gmail.com>
2024-09-09 19:53:34 -0300

    Alterações comment + system

--- loja_eletronicos.c
+++ loja_eletronicos.c
@@ -1,7 +1,10 @@
+//inclui biblioteca padrão para entrada e saída de dados
 #include <stdio.h>
+//inclui biblioteca padrão para manipulação de alocação de memória
 #include <stdlib.h>
+//inclui biblioteca padrão para manipulação de strings
 #include <string.h>
-// funcao sleep
+//inclui biblioteca da API POSIX (sleep)
 #include <unistd.h>
 
 // define tamanho das constantes
@@ -12,10 +15,9 @@
 #define COLUNAS 3
 #define TAMANHO_ID 7
 #define TAMANHO_STATUS 20
-//
-
+//declaração da senha como constante para acessar o cadastro de usuários
 const char SENHA_CADASTRO[] = "2025";
-// produto
+// struct para produto
 typedef struct
 {
     char id[TAMANHO_ID];
@@ -23,7 +25,7 @@ typedef struct
     char status[TAMANHO_STATUS];
 } Produto;
 
-// usuario
+// struct para usuário
 typedef struct
 {
     char nome[TAMANHO_NOME];
@@ -43,11 +45,11 @@ int quantidadeUsuarios = 0;
 void cadastrarUsuario()
 {
     char senha[TAMANHO_SENHA];
-
+    // recebe a senha de cadastro de usuarios
     printf("Digite a senha de cadastro: ");
     scanf("%s", senha);
-    getchar();
-
+    getchar(); // Limpar o buffer de entrada
+    // verifica se a senha de cadastro digitada é válida
     if (strcmp(senha, SENHA_CADASTRO) != 0)
     {
         printf("Senha incorreta! Retornando ao menu principal.\n");
@@ -64,22 +66,22 @@ void cadastrarUsuario()
     // recebe o nome do usuario
     printf("Digite o nome do usuario: ");
     scanf("%s", usuarios[quantidadeUsuarios].nome);
-
-    printf("Digite o cargo do usuario (Admin ou Vendedor): "); // recebe o cargo do usuario
+    // recebe o cargo do usuario
+    printf("Digite o cargo do usuario (Admin ou Vendedor): "); 
     scanf("%s", usuarios[quantidadeUsuarios].cargo);
-    getchar();
-
-    printf("Digite a senha (maximo 4 digitos): "); // recebe a senha do usuario
+    getchar(); // Limpar o buffer de entrada
+    // recebe a senha do usuario
+    printf("Digite a senha (maximo 4 digitos): "); 
     scanf("%4s", usuarios[quantidadeUsuarios].senha);
-    getchar();
+    getchar(); // Limpar o buffer de entrada
     //limpa a tela
-    system("cls");
+    system("clear || cls"); // apaga parte anterior do código na tela
     quantidadeUsuarios++; // incrementa a variavel em 1
     printf("Usuario cadastrado com sucesso!\n");
     printf("Pressione 'enter' para continuar");
-    getchar();
+    getchar(); // Limpar o buffer de entrada
     //limpa a tela
-    system("cls");
+    system("clear || cls"); // apaga parte anterior do código na tela
 }
 //retorna uma lista com todos os produtos
 void consultarProduto() {
@@ -100,8 +102,9 @@ void consultarProduto() {
     if (contador == 1) {
         printf("Nenhum produto cadastrado.\n");
     }
-}
+} // funcao para cadastar um novo produto
 void cadastrarProduto() {
+    // verifica se a quantidade de produtos excedeu o limite estabelecido.
     if (quantidadeProdutos >= LINHAS * COLUNAS) {
         printf("Limite máximo de produtos atingido!\n");
         return;
@@ -109,15 +112,15 @@ void cadastrarProduto() {
 
     int linha = quantidadeProdutos / COLUNAS;
     int coluna = quantidadeProdutos % COLUNAS;
-
+    // recebe ID do produto
     printf("Digite o ID do produto (6 digitos): ");
     scanf("%6s", produtos[linha][coluna].id);
     getchar(); // Limpar o buffer de entrada
-
+    // recebe nome do produto
     printf("Digite o nome do produto: ");
     scanf("%s", produtos[linha][coluna].nome);
     getchar(); // Limpar o buffer de entrada
-
+    // recebe status do produto
     printf("Digite o status do produto: ");
     fgets(produtos[linha][coluna].status, TAMANHO_STATUS, stdin);
     produtos[linha][coluna].status[strcspn(produtos[linha][coluna].status, "\n")] = 0; // Remover o '\n' do final
@@ -125,10 +128,10 @@ void cadastrarProduto() {
     quantidadeProdutos++;
     printf("Produto cadastrado com sucesso!\n");
     printf("Pressione 'enter' para continuar ");
-    getchar();
-    system("cls");
+    getchar(); // Limpar o buffer de entrada
+    system("clear || cls"); // apaga parte anterior do código na tela
 }
-
+// funcao para o sistema
 void Sistema()
 {
     char nome[TAMANHO_NOME];
@@ -136,17 +139,17 @@ void Sistema()
     int opcao;
     do
     {
-        system("cls");
+        system("clear || cls"); // apaga parte anterior do código na tela
         // recebe o nome do usuario já criado no sistema
         printf("Digite o nome do usuario: ");
         scanf("%s", nome);
-        getchar();
+        getchar(); // Limpar o buffer de entrada
 
         // recebe a senha do usuario já criado no sistema
         printf("Digite a senha do usuario: ");
         scanf("%s", senha);
-        getchar();
-        system("cls");
+        getchar(); // Limpar o buffer de entrada
+        system("clear || cls"); // apaga parte anterior do código na tela
 
         // verifico se o nome e senha do usuario são válidos
         for (int i = 0; i < quantidadeUsuarios; i++)
@@ -154,34 +157,45 @@ void Sistema()
             // uso strcmp para fazer a comparação do nome e senha digitado pelo usuario com os contidos no sistema
             if (strcmp(nome, usuarios[i].nome) == 0 && strcmp(senha, usuarios[i].senha) == 0)
             {
+                // Mostra Tela Inicial do Sistema com o Nome e Cargo do usuário que logou
                 printf("Bem vindo ao sistema da MDM Eletronica\n");
                 printf("Usuario: %s.\n", usuarios[i].nome);
                 printf("Cargo: %s.\n\n", usuarios[i].cargo);
                 do
                 {
+                    // Opção para cadastro de produto
                     printf("1.Cadastro Produto\n");
+                    // Opção para consulta de produto
                     printf("2.Consultar Produto\n");
+                    // Opção para exclusão de produto
                     printf("3.Excluir Produto\n");
+                    // Opção para atualização de produto
                     printf("4.Atualizar Produto\n");
+                    // Opção para sair do sistema
                     printf("5.Sair\n");
                     printf("Escolha uma opcao: ");
                     scanf("%d", &opcao);
-                    getchar();
-
+                    getchar(); // Limpar o buffer de entrada
+                    // switch case que recebe a opção selecionada pelo usuário logado
                     switch (opcao)
                     {
+                    // Opção (1) para cadastro de produto
                     case 1:
                         cadastrarProduto();
                         break;
+                    // Opção (2) para consulta de produto
                     case 2:
                         consultarProduto();
                         break;
+                    // Opção (3) para excluir um produto
                     case 3:
                         // excluir produto
                         break;
+                    // Opção (4) para atualizar um produto
                     case 4:
                         // atualizar produto
                         break;
+                    // Opção (5) para sair do sistema
                     case 5:
                         printf("Saindo do Usuario");
                         for (int i = 0; i < 3; i++)
@@ -190,7 +204,7 @@ void Sistema()
                             fflush(stdout);
                             sleep(1);
                         }
-                        system("cls");
+                        system("clear || cls"); // apaga parte anterior do código na tela
                         break;
                     default:
                         printf("Digite uma opção valida.\n");
@@ -211,26 +225,33 @@ int main()
 
     do
     {
-       
+        // Tela Inicial para o usuário
         printf("\n----------------------------------\n");
         printf("MDM Eletronica - Tela inicial\n");
+        // Opção para Cadastro de novo usuário
         printf("1. Cadastro de Usuario\n");
+        // Opção para entrar no sistema com um usuário cadastrado
         printf("2. Entrar no Sistema\n");
+        // Opção para sair da tela inicial e finalizar a execução do código
         printf("3. Sair\n");
         printf("\n----------------------------------\n");
+        // Recebe opção digitada pelo usuário
         printf("Escolha uma opcao: ");
         scanf("%d", &opcao);
-        getchar();
-        system("cls");
-
+        getchar(); // Limpar o buffer de entrada
+        system("clear || cls"); // apaga parte anterior do código na tela
+         // switch case que recebe a opção selecionada pelo usuário
         switch (opcao)
         {
+        // Opção (1) para cadastro de usuário
         case 1:
             cadastrarUsuario();
             break;
+        // Opção (2) para acessar o sistema com um usuário cadastrado
         case 2:
             Sistema();
             break;
+        // Opção (3) para sair do sistema e finalizar a execução do código
         case 3:
             printf("Saindo do Sistema");
             for (int i = 0; i < 3; i++)
@@ -241,10 +262,10 @@ int main()
             }
             break;
         default:
-            printf("Porfavor, selecione uma opcao valida\n");
+            printf("Por favor, selecione uma opcao valida\n");
             printf("Pressione enter para continuar\n");
-            getchar();
-            system("cls");
+            getchar(); // Limpar o buffer de entrada
+            system("clear || cls"); // apaga parte anterior do código na tela
             break;
         }
         
